a074ecbd67b7884d9d8142447e3eeafdcd08b0de
neagkv <neagkv@gmail.com>
2018-07-26 17:03:16 -0400

    new repo

---
+++ src/main/java/com/dev/SOAPAPI/SoapApiApplication.java
@@ -0,0 +1,12 @@
+package com.dev.SOAPAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/dev/SOAPAPI/SoapApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dev.SOAPAPI;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SoapApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
